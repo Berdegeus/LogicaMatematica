e0aa02638e0a8c1ea769e0ce8e23027f7939bdfb
Bernardo <bernardo.geus@pucpr.edu.br>
2023-03-14 22:54:02 -0300

    adicionando GUI
    depois de alguns erros, adicionei uma UI com tkinter

--- Codes/Tabela Verdade/tabela verdade.py
+++ Codes/Tabela Verdade/tabela verdade.py
@@ -1,4 +1,5 @@
 import itertools
+import tkinter as tk
 
 # definir operadores lógicos
 def E(*args):
@@ -13,32 +14,31 @@ def NAO(a):
 def OU_EXCLUSIVO(a, b):
     return (a and not b) or (not a and b)
 
-# obter entrada do usuário
-n = int(input("Número de Variáveis: "))
-opcoes_operacao = ["E", "OU", "NÃO", "OU_EXCLUSIVO"]
-operacao = input("Operação: " + ", ".join(opcoes_operacao) + "\n").upper()
+# definir função de tabela verdade
+def tabela_verdade(operacao, n):
+    table = tk.Toplevel(root)
+    table.title(operacao.__name__)
+    for i in range(n):
+        tk.Label(table, text=f"{'A' if i == 0 else chr(65+i)}").grid(row=0, column=i)
+    tk.Label(table, text="Resultado").grid(row=0, column=n)
+    tk.Label(table, text="-" * 20).grid(row=1, column=0, columnspan=n+1)
+    for values in itertools.product((True, False), repeat=n):
+        resultado = operacao(*values)
+        row = len(table.grid_slaves()) // (n+1) + 2
+        for i, value in enumerate(values):
+            tk.Label(table, text=str(value)).grid(row=row, column=i)
+        tk.Label(table, text=str(resultado)).grid(row=row, column=n)
 
-# verificar entrada do usuário
-if operacao not in opcoes_operacao:
-    print("Operação inválida.")
-    exit()
+# criar interface gráfica
+root = tk.Tk()
+root.title("Tabelas Verdade")
+tk.Label(root, text="Número de Variáveis:").grid(row=0, column=0)
+var_entry = tk.Entry(root)
+var_entry.grid(row=0, column=1)
+tk.Button(root, text="E", command=lambda: tabela_verdade(E, int(var_entry.get()))).grid(row=1, column=0)
+tk.Button(root, text="OU", command=lambda: tabela_verdade(OU, int(var_entry.get()))).grid(row=1, column=1)
+tk.Button(root, text="NÃO", command=lambda: tabela_verdade(NAO, 1)).grid(row=2, column=0)
+tk.Button(root, text="OU_EXCLUSIVO", command=lambda: tabela_verdade(OU_EXCLUSIVO, int(var_entry.get()))).grid(row=2, column=1)
 
-# gerar tabela verdade
-print()
-for i in range(n):
-    print(f"{'A' if i == 0 else chr(65+i)}", end="\t")
-print("Resultado")
-print("-" * (n*8 + 9))
-for values in itertools.product((True, False), repeat=n):
-    resultado = None
-    if operacao == "E":
-        resultado = E(*values)
-    elif operacao == "OU":
-        resultado = OU(*values)
-    elif operacao == "NÃO":
-        resultado = NAO(values[0])
-    elif operacao == "OU_EXCLUSIVO":
-        resultado = OU_EXCLUSIVO(*values)
-    for value in values:
-        print(str(value), end="\t")
-    print(str(resultado))
+# iniciar loop de eventos
+root.mainloop()
